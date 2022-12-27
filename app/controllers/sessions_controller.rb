bf306fb97c067a9e50bdf81dd626fda8ecdf6e82
vampire-yuta <yuta@vamdemicsystem.black>
2022-12-27 19:40:35 +0900

    finish 10.34

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,13 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     # ユーザーがデータベース内に存在し、かつ、認証に成功した場合
     if @user && @user&.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
       # ユーザーログイン後にユーザー情報のページへリダイレクトする
-      # reset_session #ログイン前に固定セッションをリセットする
+      reset_session #ログイン前に固定セッションをリセットする
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      # redirect_to @user
+      redirect_to forwarding_url || @user
     else
       # エラーメッセージを表示する 
       flash.now[:danger] = "Invalid email/password combination" #本当は正しくない
