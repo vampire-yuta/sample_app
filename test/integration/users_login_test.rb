06d2c8bcb725c6a75197c19436a8533821e290d9
vampire-yuta <yuta@vamdemicsystem.black>
2022-12-28 00:32:28 +0900

    Finish 11.34

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -58,16 +58,6 @@ class Logout < ValidLogin
   end
 end
 
-
-  # test "login with valid information" do
-  #   post login_path, params: { session: { email: @user.email, password: 'password' } }
-  #   assert_redirected_to @user
-  #   follow_redirect!
-  #   assert_select "a[href=?]", login_path, count: 0
-  #   assert_select "a[href=?]", logout_path
-  #   assert_select "a[href=?]", user_path(@user)
-  # end
-
 class LogoutTest < Logout
 
   test "successful logout" do
