e38a8a91dbd2e56be417d7a0ccc0447e4fa640a1
vampire-yuta <yuta@vamdemicsystem.black>
2022-12-28 13:59:30 +0900

    Finish 11.44

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,8 +48,9 @@ class User < ApplicationRecord
 
   # Acivate account
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    # update_attribute(:activated, true)
+    # update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # Sending mail for activation
