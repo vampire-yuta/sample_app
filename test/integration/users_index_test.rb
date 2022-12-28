3df6853f1dec3d5722f04c6c3ea2cc26defa7e6d
vampire-yuta <yuta@vamdemicsystem.black>
2022-12-28 13:59:30 +0900

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,23 +1,40 @@
 require "test_helper"
 
-class UsersIndexTest < ActionDispatch::IntegrationTest
+class UsersIndex < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
   end
+end
 
-  test "index as admin including pagination and delete links" do
+class UsersIndexAdmin < UsersIndex
+  def setup
+    super
     log_in_as(@admin)
     get users_path
+  end
+end
+
+class UsersIndexAdminTest < UsersIndexAdmin
+  test "should render the index page" do
     assert_template 'users/index'
+  end
+
+  test "should paginate users" do
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+  end
+
+  test "should hav delete links" do
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]' , user_path(user), text: 'delete'
       end
     end
+  end
+
+  test "should be able to delete non-admin user" do
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
       assert_response :see_other
@@ -25,6 +42,17 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "should display only activated users" do
+    # disabled first user of pages
+    User.paginate(page: 1).first.toggle!(:activated)
+    get users_path
+    assigns(:users).each do |user|
+      assert user.activate
+    end
+  end
+end
+
+class UsersNonAdminIndexTest < UsersIndex
   test "index as non-admin" do
     log_in_as(@non_admin)
     get users_path
