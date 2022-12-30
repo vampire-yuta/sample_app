595e7f0b29a0ec00cd7836296e9869004ab783fe
vampire-yuta <yuta@vamdemicsystem.black>
2022-12-31 05:01:41 +0900

    add cache clear command

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
+rm tmp/cache/bootsnap-load-path-cache
+rm -rf tmp/cache/bootsnap
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
