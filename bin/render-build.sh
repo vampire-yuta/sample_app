6e241a83049d23b5e80d96254a879619ed57227a
vampire-yuta <yuta@vamdemicsystem.black>
2022-12-31 06:08:38 +0900

    application_helper_test. moved directory

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 # exit on error
+
+echo $RAILS_ENV
+echo '----------'
 set -o errexit
 
 rm -rf tmp/cache/
 
 bundle install
+# bundle install --without test development
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate:reset
