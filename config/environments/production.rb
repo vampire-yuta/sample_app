8a1257e48efa181b82ce057c26f0fb697f251257
vampire-yuta <yuta@vamdemicsystem.black>
2023-01-10 03:02:56 +0900

    test change mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,26 +66,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'hello-app-zp2g.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
     :address        => 'smtp.mailgun.org',
-    :user_name      => 'postmaster@sandbox86ca8147239149b08d308ce14ea31254.mailgun.org',
-    :password       => 'd597ee2e2595ac724701949706924d40-c2efc90c-0dc0e74f',
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => host,
     :authentication => :plain,
   }
-  # ActionMailer::Base.smtp_settings = {
-  #   :port           => 587,
-  #   :address        => 'smtp.mailgun.org',
-  #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  #   :domain         => host,
-  #   :authentication => :plain,
-  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
